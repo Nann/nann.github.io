0a698a317dc54c0e427e376ce2baf372218ee1ea
Nann <nannchalermchai@gmail.com>
2017-08-13 16:17:19 +0700

    Add resume with pwa

---
+++ sw.js
@@ -0,0 +1,45 @@
+// ./sw.js
+var cacheName = 'starter-project-nannstudio';
+var filesToCache = [
+  '/index.html',
+  '/assets/vendor/bulma/0.5.0/css/bulma.min.css',
+  /* ...and other assets (jQuery, Materialize, fonts, etc) */
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  //...
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
