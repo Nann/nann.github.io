6a491bb977f20a6f024228e08574263a9023d55a
Nann <nannchalermchai@gmail.com>
2017-08-14 17:07:33 +0700

    Fix Service Worker

--- sw.js
+++ sw.js
@@ -1,51 +1,36 @@
-// ./sw.js
-var cacheName = 'Resume NannStudio';
-var filesToCache = [
-  '/index.html',
-  '/assets/vendor/normalize/7.0.0/css/normalize.min.css',
-  '/assets/vendor/bootstrap/3.3.7/css/bootstrap.min.css',
-  '/assets/vendor/font-awesome/4.7.0/css/font-awesome.min.css',
-  '/assets/vendor/animate/3.5.2/css/animate.min.css',
-  '/assets/css/mousescroll.min.css',
-  '/assets/css/custom.min.css',
-  '/assets/img/profile-512x512.png',
-  /* ...and other assets (jQuery, Materialize, fonts, etc) */
-];
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+// Install the service worker.
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('nannstudio').then(function(cache) {
+      // The cache will fail if any of these resources can't be saved.
+      return cache.addAll([
+        // Path is relative to the origin, not the app directory.
+        '/index.html',
+        '/assets/vendor/normalize/7.0.0/css/normalize.min.css',
+        '/assets/vendor/bootstrap/3.3.7/css/bootstrap.min.css',
+        '/assets/vendor/font-awesome/4.7.0/css/font-awesome.min.css',
+        '/assets/vendor/animate/3.5.2/css/animate.min.css',
+        '/assets/css/custom.min.css',
+        '/assets/img/profile-512x512.png',
+        '/manifest.json'
+      ])
+      .then(function() {
+        console.log('Success! App is available offline!');
+      })
     })
   );
 });
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  //...
-});
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+// Define what happens when a resource is requested.
+// For our app we do a Cache-first approach.
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      // Try the cache.
+      caches.match(event.request)
+      .then(function(response) {
+      // Fallback to network if resource not stored in cache.
+      return response || fetch(event.request);
     })
-  );
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    );
 });
